49bb5a1d8197c3e786977a9ad7dff029516a0779
LuisCRA <insaneyourmother@gmail.com>
2017-04-29 11:21:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -69,14 +69,14 @@
 
     <!-- Page Header -->
     <!-- Set your background image for this header on the line below. -->
-    <header class="intro-header" style="background-image: url('images/baner 1.png')">
+    <header class="intro-header" style="background-image: url('images/cafe.jpg')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <div class="site-heading">
-                        <h1>Clean Blog</h1>
+                        <h1></h1>
                         <hr class="small">
-                        <span class="subheading">A Clean Blog Theme by Start Bootstrap</span>
+                        <span class="subheading"></span>
                     </div>
                 </div>
             </div>
